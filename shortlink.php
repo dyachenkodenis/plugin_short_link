a442f2ae8a8f8fd4a49cd6b676432acd6adc82d2
dyachenkodenis <denisdyachenko.90@gmail.com>
2024-09-19 06:00:10 +0500

    add textdomain comment

---
+++ shortlink.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Short Links Plugin
+Description: Плагин для создания коротких ссылок 
+Version: 1.0
+Author: Denis
+Text Domain: shortlink
+*/
+
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
+App\ShortLinksPlugin::get_instance();
